647cea834ab63ca23291d3c6b107448e3b7d4afb
Sin Kim <bye_ok@naver.com>
2023-09-17 11:35:07 +0900

    [redux]

--- src/client/js/redux.js
+++ src/client/js/redux.js
@@ -11,23 +11,30 @@ function subject() {
 function TOC() {
   let state = store.getState();
   let i = 0;
-  let liTags = "";
+  const NAV = document.createElement("nav");
+  const OL = document.createElement("ol");
+
   while (i < state.contents.length) {
-    liTags =
-      liTags +
-      `
-      <li>
-        <a href="${state.contents[i].id}".html>${state.contents[i].title}</a>
-      </li>
-    `;
+    let li = document.createElement("li");
+    let btn = document.createElement("a");
+    btn.innerText = state.contents[i].title;
+    btn.classList.add("btn-toc");
+    li.appendChild(btn);
+    OL.appendChild(li);
     i = i + 1;
   }
+  NAV.appendChild(OL);
+  document.querySelector("#toc").innerHTML = "";
+  document.querySelector("#toc").appendChild(NAV);
 
-  document.querySelector("#toc").innerHTML = `
-    <nav>
-      <ol>${liTags}</ol>
-    </nav>
-  `;
+  const BTNS = document.querySelectorAll(".btn-toc");
+  for (let idx = 0; idx < BTNS.length; idx++) {
+    BTNS[idx].onclick = (event) => {
+      event.preventDefault();
+      let action = { type: "SELECT", id: state.contents[idx].id };
+      store.dispatch(action);
+    };
+  }
 }
 function control() {
   document.querySelector("#control").innerHTML = `
@@ -38,25 +45,102 @@ function control() {
   `;
 }
 function article() {
-  document.querySelector("#content").innerHTML = `
-    <article>
-      <h2>HTML</h2>
-      HTML is ...
-    </article>
-  `;
+  let state = store.getState();
+
+  if (state.mode === "create") {
+    const ARTICLE = document.createElement("article");
+    const FORM = document.createElement("form");
+    const P1 = document.createElement("p");
+    const P2 = document.createElement("p");
+    const P3 = document.createElement("p");
+    const INPUT_T = document.createElement("input");
+    const INPUT_S = document.createElement("input");
+    const TEXTAREA = document.createElement("textarea");
+
+    INPUT_T.type = "text";
+    INPUT_T.name = "title";
+    INPUT_T.placeholder = "text";
+    TEXTAREA.name = "desc";
+    TEXTAREA.placeholder = "description";
+    INPUT_S.type = "submit";
+
+    P1.appendChild(INPUT_T);
+    P2.appendChild(TEXTAREA);
+    P3.appendChild(INPUT_S);
+    FORM.appendChild(P1);
+    FORM.appendChild(P2);
+    FORM.appendChild(P3);
+    ARTICLE.appendChild(FORM);
+    document.querySelector("#content").appendChild(ARTICLE);
+
+    const FORM_EL = document.querySelector("form");
+
+    FORM_EL.onsubmit = (event) => {
+      event.preventDefault();
+      let _title = event.target.querySelector("input[name='title']").value;
+      let _desc = event.target.querySelector("textarea[name='desc']").value;
+      store.dispatch({
+        type: "CREATE",
+        title: _title,
+        desc: _desc,
+      });
+    };
+  } else if (state.mode === "read") {
+    let aTitle, aDesc;
+    let i = 0;
+    while (i < state.contents.length) {
+      if (state.contents[i].id === state.selected_id) {
+        aTitle = state.contents[i].title;
+        aDesc = state.contents[i].desc;
+        break;
+      }
+      i = i + 1;
+    }
+    document.querySelector("#content").innerHTML = `
+      <article>
+        <h2>${aTitle}</h2>
+        ${aDesc}
+      </article>
+    `;
+  }
 }
 
 function reducer(state, action) {
   if (state === undefined) {
     return {
+      max_id: 2,
+      mode: "create",
+      selected_id: 1,
       contents: [
-        { id: 1, title: "HTML", disc: "HTML is ..." },
-        { id: 2, title: "CSS", disc: "CSS is ..." },
+        { id: 1, title: "HTML", desc: "HTML is ..." },
+        { id: 2, title: "CSS", desc: "CSS is ..." },
       ],
     };
   }
+  let newState;
+  if (action.type === "SELECT") {
+    newState = Object.assign({}, state, { selected_id: action.id });
+  } else if (action.type === "CREATE") {
+    var newMaxId = state.max_id + 1;
+    var newContents = state.contents.concat();
+    newContents.push({
+      id: newMaxId,
+      title: action.title,
+      desc: action.desc,
+    });
+    newState = Object.assign({}, state, {
+      max_id: newMaxId,
+      contents: newContents,
+      mode: "read",
+    });
+  }
+  console.log(action, state, newState);
+  return newState;
 }
 let store = createStore(reducer);
+
+store.subscribe(article);
+store.subscribe(TOC);
 
 subject();
 TOC();
